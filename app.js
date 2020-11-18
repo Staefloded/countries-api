3cfd8173d87e14c083844ccb5488c43a045095dd
Staefloded <31619471+Staefloded@users.noreply.github.com>
2020-11-18 01:37:08 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
-async function getCountries() {
-  const response = await fetch("https://restcountries.eu/rest/v2/all");
-  const data = await response.json();
-  let output = "";
-  data.forEach((country) => {
-    output += `<div class="country">
+const countryContainer = document.querySelector(".output");
+const spinner = document.querySelector("#spinner");
+const input = document.querySelector("input");
+const container = document.querySelector(".container");
+
+const getCountries = async () => {
+  try {
+    spinner.removeAttribute("hidden");
+    const response = await fetch("https://restcountries.eu/rest/v2/all");
+    const data = await response.json();
+
+    let output = "";
+    data.forEach((country) => {
+      const { flag, name, capital, region, languages, subregion, population } = country;
+
+      output += `<div class="country">
 		<div class="img--container">
-			<img class="country--img" src=${country.flag} alt="Flag">
+			<img class="country--img" src=${flag} alt="Flag">
 		</div>
 		<div class="country--text">
-			<h2>${country.name}</h2>
-			<p>Capital: ${country.capital}</p>
-			<p>Region: ${country.region}</p>
-			<p>Languages: ${country.languages.map((language) => language.name)}</p>
-			<p>Subregion: ${country.subregion}</p>
-			<p>Region: ${country.region}</p>
-			<p>Population: ${country.population}</p>
+      <h2 class="country--name">${name}</h2>
+      
+			<p><span class="title">Capital:</span> ${
+        capital
+          ? `<span class="small">${capital}</span>`
+          : "<span class='error'>Not Available</span>"
+      }</p>
+
+      	<p><span class="title">Region:</span> ${
+          region
+            ? `<span class="small">${region}</span>`
+            : "<span class='error'>Not Available</span>"
+        }</p>
+      
+			<p><span class="title">Languages:</span> ${
+        languages
+          ? `<span class="small">${languages.map((language) => language.name)}</span>`
+          : "<span class='error'>Not Available</span>"
+      }</p>
+
+      	<p><span class="title">Subregion:</span> ${
+          subregion
+            ? `<span class="small">${subregion}</span>`
+            : "<span class='error'>Not Available</span>"
+        }</p>
+      
+			<p><span class="title">Population:</span> ${numberWithCommas(population)}</p>
 		</div>
 	</div>`;
-  });
-  document.querySelector(".output").innerHTML = output;
-  return data;
-}
+    });
+    countryContainer.innerHTML = output;
+    return data;
+  } catch (error) {
+    const div = document.createElement("div");
+    div.className = "country--error";
+    const para = document.createElement("p");
+    para.innerHTML = `<i class="fas fa-exclamation-circle"></i> ${error}`;
+    div.appendChild(para);
+    container.appendChild(div);
+  }
+};
 
-getCountries()
-  .then((countries) => console.log(countries))
-  .catch((err) => console.log(err));
+getCountries().then(() => setTimeout(spinner.setAttribute("hidden", "")));
+
+// Separate Population with Comma
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
